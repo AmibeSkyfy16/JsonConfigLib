eb88d6c3570380b192b30cc2e3b7ece8472bfabd
skyfy16 <colin.viatte@gmail.com>
2022-09-14 13:31:34 +0200

    Trying to add delegate for all nested members

--- src/main/kotlin/ch/skyfy/jsonconfiglib/ConfigData.kt
+++ src/main/kotlin/ch/skyfy/jsonconfiglib/ConfigData.kt
@@ -3,18 +3,16 @@ package ch.skyfy.jsonconfiglib
 import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.properties.Delegates
-import kotlin.reflect.KMutableProperty0
-import kotlin.reflect.KMutableProperty1
-import kotlin.reflect.KProperty
-import kotlin.reflect.KProperty1
+import kotlin.reflect.*
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.starProjectedType
 
 /**
  * Set the value for the chosen member property by accessing her delegate
  */
 inline fun <reified DATA : Validatable, reified TYPE> ConfigData<DATA>.setValue(kProperty0: KMutableProperty0<TYPE>, value: TYPE) {
     delegates.forEach { entry ->
-        if (entry.key.name == kProperty0.name){
+        if (entry.key.name == kProperty0.name) {
             val anonymous = entry.value.invoke().get()
             val delegated = anonymous::class.memberProperties.first() as KMutableProperty1
             delegated.setter.call(anonymous, value)
@@ -47,8 +45,8 @@ data class ConfigData<DATA : Validatable>(
     data class InitializeObject<DATA : Validatable>(
         val delegates: MutableMap<KProperty1<*, *>, () -> AtomicReference<Any>>,
         val onChangeCallbacks: MutableMap<KProperty1<*, *>, MutableList<(KProperty<*>, Any?, Any?, DATA) -> Unit>>
-    ){
-        companion object{
+    ) {
+        companion object {
             inline operator fun <reified DATA : Validatable> invoke(
                 delegates: MutableMap<KProperty1<*, *>, () -> AtomicReference<Any>>,
                 onChangeCallbacks: MutableMap<KProperty1<*, *>, MutableList<(KProperty<*>, Any?, Any?, DATA) -> Unit>>
@@ -70,12 +68,61 @@ data class ConfigData<DATA : Validatable>(
             return ConfigData(`data`, relativeFilePath, initializeObject.delegates, initializeObject.onChangeCallbacks)
         }
 
+        fun getBasePackageName(string: String) : String{
+            var count = 0
+            string.toCharArray().forEachIndexed { index, c ->
+                if(c.toString() == "."){
+                    if(count == 1){
+                        @Suppress("UnnecessaryVariable") val basePackageName = string.substring(0, index)
+                        return basePackageName
+                    }
+                    count++
+                }
+            }
+            return string
+        }
+
         /**
          * Create delegate for each member property found in [DATA]
          */
         inline fun <reified DATA : Validatable> initialize(`data`: DATA, relativeFilePath: Path, automaticallySave: Boolean): InitializeObject<DATA> {
+
             val delegates = mutableMapOf<KProperty1<*, *>, () -> AtomicReference<Any>>()
             val onChangeCallbacks: MutableMap<KProperty1<*, *>, MutableList<(KProperty<*>, Any?, Any?, DATA) -> Unit>> = mutableMapOf()
+
+            val basePackageName = getBasePackageName(DATA::class.java.`package`.name)
+
+            DATA::class.memberProperties.forEach {kp1 ->
+                kp1.returnType.arguments.forEach {kTypeProjection ->
+                    val nestedMemberPackageName = kTypeProjection.toString()
+
+                    if(getBasePackageName(nestedMemberPackageName) == basePackageName) {
+                        val JClass = kTypeProjection.type?.javaClass?.kotlin
+                        JClass?.memberProperties?.forEach { kp2 ->
+
+                            println()
+                        }
+                        println()
+                    }
+                }
+//                val all = it.get(data)!!::class.allSupertypes
+//                all.forEach { p ->
+//                    p.arguments.forEach { kTypeProjection ->
+//                        val t = kTypeProjection.type
+//                        println()
+//                    }
+//                }
+//
+//                val nestMembers = it.get(data)?.javaClass?.nestMembers
+//                nestMembers?.forEach { jClass ->
+//                    val p2 = jClass.`package`
+//                    println()
+//                }
+                println()
+            }
+
+//            doStuff(data, relativeFilePath, automaticallySave, delegates, onChangeCallbacks)
+
             DATA::class.memberProperties.forEach { kProperty1 ->
                 val anonymous = object {
                     @Suppress("unused")
@@ -92,7 +139,33 @@ data class ConfigData<DATA : Validatable>(
                 if (automaticallySave) mutableListOf.add { _, _, _, _ -> ConfigManager.save(`data`, relativeFilePath) }
                 onChangeCallbacks[kProperty1] = mutableListOf
             }
+
             return InitializeObject.invoke(delegates, onChangeCallbacks)
+        }
+
+        inline fun <reified DATA : Validatable> doStuff(
+            data: DATA,
+            relativeFilePath: Path,
+            automaticallySave: Boolean,
+            delegates: MutableMap<KProperty1<*, *>, () -> AtomicReference<Any>>,
+            onChangeCallbacks: MutableMap<KProperty1<*, *>, MutableList<(KProperty<*>, Any?, Any?, DATA) -> Unit>>
+        ) {
+            DATA::class.memberProperties.forEach { kProperty1 ->
+                val anonymous = object {
+                    @Suppress("unused")
+                    var delegated by Delegates.observable(kProperty1.get(`data`)) { kProperty, oldValue, newValue ->
+                        if (kProperty1 is KMutableProperty1) kProperty1.setter.call(`data`, newValue)
+                        onChangeCallbacks.forEach { entry ->
+                            if (entry.key === kProperty1) entry.value.forEach { it.invoke(kProperty, oldValue, newValue, data) }
+                        }
+                    }
+                }
+                delegates[kProperty1] = { AtomicReference(anonymous) }
+
+                val mutableListOf: MutableList<(KProperty<*>, Any?, Any?, DATA) -> Unit> = mutableListOf()
+                if (automaticallySave) mutableListOf.add { _, _, _, _ -> ConfigManager.save(`data`, relativeFilePath) }
+                onChangeCallbacks[kProperty1] = mutableListOf
+            }
         }
     }
 
